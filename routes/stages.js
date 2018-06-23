30ded65133f4f583fc2317ed752cccd983edb273
kira076 <j.morrisoncleary@gmail.com>
2018-06-23 22:32:22 +1000

    maybe actually fixed urls this time

--- routes/stages.js
+++ routes/stages.js
@@ -48,7 +48,7 @@ router.get('/delete/:stageID', function(req, res){
 });
 
 router.get('/new/:jsonObj', function(req, res){
-  var data = JSON.parse(atob(req.params.jsonObj));
+  var data = JSON.parse(Buffer(req.params.jsonObj).tostring());
   var credlist;
 
   CreditModel.find({}, '_id credit name').exec()
